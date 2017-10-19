d3f6f289b6aa156ee7910744353e9445809e86a5
marymatt <marylin.mattos@gmail.com>
2017-10-19 17:56:27 +0200

    Heroku OK

---
+++ DAO/conexionMongo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from os import environ
+
+def getConexion():
+    """Método que contine la cadena de conexión a la base de datos
+
+    La base de datos se encuentra alojada en mlab que es un servicio en la nube que
+    hospeda BD de MongoDB
+    """
+
+
+    return environ.get("RUTA_MONGO")
+
+#
+def getDB():
+    """Método que retorna el nombre de la BD que se está usando.
+    """
+    return 'db_tweets'
+
+def getCollTweetsClas():
+    """Devuelve el nombre de la colección Tweets Clasificados.
+
+     Retorna el nombre de la colección que contiene los tweets
+     clasificados.
+
+     Justificación: Estos métodos se crear para que  sea fácil el
+     mantenamiento del nombre de las colecciones.
+
+    """
+    return 'TWCLASIFICADO'
