18312a24055af966aafdf16cd4c7bbe2dd6730b4
marymatt <marylin.mattos@gmail.com>
2017-10-19 18:53:41 +0200

    Pruebas

--- BLL/catalogoBLL.py
+++ BLL/catalogoBLL.py
@@ -90,7 +90,9 @@ def getCatalogoBLL(anyo, mes):
 
     #Si el fichero existe, lo devuelve.
     print os.getcwd()
-    print os.path.isfile("/FILES/"+filename)
+    print os.path.isfile(filename)
+    print os.path.isfile("app/FILES/"+filename)
+
     if os.path.isfile("/FILES/"+filename):
         return calculoCatalogo(filename)
     else:
